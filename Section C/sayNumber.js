b7fcf8e2b3218ee816779e481fb3d3377cc5135a
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2023-02-03 09:12:31 +0200

    Refactor say number function

--- Section C/sayNumber.js
+++ Section C/sayNumber.js
@@ -1,60 +1,91 @@
+// function sayNumber(num) {
+//   // An array of words to represent numbers from 0 to 19
+//   const numWords = [    "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",    "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"  ];
+
+//   // An array of words to represent tens from 20 to 90
+//   const tensWords = [    "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"  ];
+
+//   // Convert the input numeral to a string and get its length
+//   let numStr = num.toString();
+//   let numLen = numStr.length;
+
+//   // Initialize an empty result string
+//   let result = "";
+
+//   // If the input numeral is less than 20, return the corresponding word
+//   if (num < 20) {
+//     result = numWords[num];
+//   }
+//   // If the input numeral has 2 digits, handle it as a tens and units combination
+//   else if (numLen === 2) {
+//     let tens = parseInt(numStr[0]);
+//     let units = parseInt(numStr[1]);
+//     result = tensWords[tens - 2];
+//     if (units !== 0) {
+//       result += " " + numWords[units];
+//     }
+//   }
+//   // If the input numeral has more than 2 digits, handle it as a combination of thousands, hundreds, tens and units
+//   else {
+//     let thousands = parseInt(numStr[0]);
+//     let hundreds = parseInt(numStr[numLen - 3]);
+//     let tens = parseInt(numStr[numLen - 2]);
+//     let units = parseInt(numStr[numLen - 1]);
+//     if (thousands > 0) {
+//       result += numWords[thousands] + " Thousand";
+//     }
+//     if (hundreds > 0) {
+//       result += " " + numWords[hundreds] + " Hundred";
+//     }
+//     if (tens > 0 || units > 0) {
+//       result += " ";
+//       if (tens < 2) {
+//         result += numWords[tens * 10 + units];
+//       } else {
+//         result += tensWords[tens - 2];
+//         if (units > 0) {
+//           result += " " + numWords[units];
+//         }
+//       }
+//     }
+//   }
+
+//   // Add a period at the end of the result string
+//   result += ".";
+
+//   // Return the result string
+//   return result;
+// }
+
+// console.log(sayNumber(9001))
+
 function sayNumber(num) {
-  // An array of words to represent numbers from 0 to 19
-  const numWords = [    "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",    "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"  ];
-
-  // An array of words to represent tens from 20 to 90
-  const tensWords = [    "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"  ];
-
-  // Convert the input numeral to a string and get its length
-  let numStr = num.toString();
-  let numLen = numStr.length;
-
-  // Initialize an empty result string
+  if (num === 0) return "Zero.";
+  
   let result = "";
+  const ones = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"];
+  const tens = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"];
+  const places = ["", "Thousand", "Million", "Billion", "Trillion"];
 
-  // If the input numeral is less than 20, return the corresponding word
-  if (num < 20) {
-    result = numWords[num];
-  }
-  // If the input numeral has 2 digits, handle it as a tens and units combination
-  else if (numLen === 2) {
-    let tens = parseInt(numStr[0]);
-    let units = parseInt(numStr[1]);
-    result = tensWords[tens - 2];
-    if (units !== 0) {
-      result += " " + numWords[units];
+  let i = 0;
+  while (num > 0) {
+    let n = num % 1000;
+    let temp = "";
+    if (n >= 100) {
+      temp += ones[Math.floor(n/100)] + " Hundred ";
+      n %= 100;
     }
-  }
-  // If the input numeral has more than 2 digits, handle it as a combination of thousands, hundreds, tens and units
-  else {
-    let thousands = parseInt(numStr[0]);
-    let hundreds = parseInt(numStr[numLen - 3]);
-    let tens = parseInt(numStr[numLen - 2]);
-    let units = parseInt(numStr[numLen - 1]);
-    if (thousands > 0) {
-      result += numWords[thousands] + " Thousand";
+    if (n >= 20) {
+      temp += tens[Math.floor(n/10)] + " ";
+      n %= 10;
     }
-    if (hundreds > 0) {
-      result += " " + numWords[hundreds] + " Hundred";
-    }
-    if (tens > 0 || units > 0) {
-      result += " ";
-      if (tens < 2) {
-        result += numWords[tens * 10 + units];
-      } else {
-        result += tensWords[tens - 2];
-        if (units > 0) {
-          result += " " + numWords[units];
-        }
-      }
-    }
+    temp += ones[n];
+    result = (temp + " " + places[i] + " " + result).trim();
+    i++;
+    num = Math.floor(num/1000);
   }
 
-  // Add a period at the end of the result string
-  result += ".";
-
-  // Return the result string
-  return result;
+  return result + ".";
 }
 
-console.log(sayNumber(9001))
+console.log(sayNumber(9400))
